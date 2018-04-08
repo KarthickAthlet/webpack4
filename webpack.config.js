bcb90e392a73484e8e5fcbe062e7cf7ba0e63f5c
karthikeyan N P <karthick.psgtec12@gmail.com>
2018-04-08 11:55:09 +0530

    Added HTML loader and HTML webpack plugin
    Added HTML loader and HTML webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,42 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+// module.exports = {
+//     module: {
+//         rules: [{
+//             test: /\.(js|jsx)$/,
+//             exclude: /node_modules/,
+//             use: {
+//                 loader: "babel-loader"
+//             }
+//         }]
+//     }
+// };
+
 module.exports = {
     module: {
-        rules: [{
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            use: {
-                loader: "babel-loader"
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        },
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: "html-loader",
+              options: { minimize: true }
             }
-        }]
-    }
-};
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: "./src/index.html",
+        filename: "./index.html"
+      })
+    ]
+  };
