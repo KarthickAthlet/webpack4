0b996882d6935aee835f895a9ceafe9fcfaf087c
karthikeyan N P <karthick.psgtec12@gmail.com>
2018-04-08 11:44:13 +0530

    Added react js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     module: {
         rules: [{
-            test: /\.js$/,
+            test: /\.(js|jsx)$/,
             exclude: /node_modules/,
             use: {
                 loader: "babel-loader"
