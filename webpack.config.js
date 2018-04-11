2247e19cce16eaaaba4ceba14e00fb48c4e8ee3a
Karthikeyan Palanisamy <kpalan@sapient.com>
2018-04-11 16:07:13 +0530

    Added Scss plugin
    Added Scss plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,13 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
+// const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+// const extractSass = new ExtractTextPlugin({
+//     filename: "[name].[contenthash].css",
+//     disable: process.env.NODE_ENV === "development"
+// });
+
 module.exports = {
     module: {
         rules: [{
